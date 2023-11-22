8db527facc76703b3468a933d9d8afbeb8b7ffd1
Ram Caneda <ram-angelo.caneda@takeda.com>
2023-11-22 18:21:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
